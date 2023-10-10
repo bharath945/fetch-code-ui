ac91f3d78318467aec615fa936136fee78e849c1
SaiBharath-Bungee <saibharath@bungeetech.com>
2023-10-10 20:15:45 +0530

    added amination

--- src/components/BodyContent/BodyContent.js
+++ src/components/BodyContent/BodyContent.js
@@ -5,12 +5,14 @@ import Fade from 'react-reveal/Fade';
 export default function BodyContent() {
     return (
         <Row className='bodyContent'>
-            <Row className='firstPara'>
-                Introducing FetchCode, a leading software company based in Hyderabad, TG. We specialize in developing innovative solutions that empower businesses to thrive in the digital age. With our cutting-edge technology and expertise, we help companies streamline their operations, enhance productivity, and drive growth.
-            </Row>
-            <Row className='mt-5'>
-                At FetchCode, we understand the ever-evolving needs of businesses and provide tailored software solutions to meet their unique requirements. Our team of skilled professionals is dedicated to delivering top-notch services, ensuring seamless integration, and providing exceptional customer support. Whether you need custom software development, mobile app solutions, or web development services, FetchCode is your trusted partner for success. Experience the power of technology with FetchCode and unlock your business's full potential.
-            </Row>
+            <Fade bottom>
+                <Row className='firstPara'>
+                    Introducing FetchCode, a leading software company based in Hyderabad, TG. We specialize in developing innovative solutions that empower businesses to thrive in the digital age. With our cutting-edge technology and expertise, we help companies streamline their operations, enhance productivity, and drive growth.
+                </Row>
+                <Row className='mt-5'>
+                    At FetchCode, we understand the ever-evolving needs of businesses and provide tailored software solutions to meet their unique requirements. Our team of skilled professionals is dedicated to delivering top-notch services, ensuring seamless integration, and providing exceptional customer support. Whether you need custom software development, mobile app solutions, or web development services, FetchCode is your trusted partner for success. Experience the power of technology with FetchCode and unlock your business's full potential.
+                </Row>
+            </Fade>
         </Row>
     );
 }
